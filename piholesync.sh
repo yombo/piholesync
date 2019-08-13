a0f24239b04e8ab682c6f72cd52806d1f1fc4a8b
Mitch Schwenk <mitch@yombo.net>
2019-08-12 22:58:07 -0700

    Add more display items. Fix bad/copy paste for original pihole script. Change sleep time to 30 seconds as the default.

--- piholesync.sh
+++ piholesync.sh
@@ -7,13 +7,13 @@ PIHOLE2=192.168.1.3 #IP of 2nd PiHole
 HAUSER=root #user of second pihole
 TICK="[\e[32m ✔ \e[0m]"
 UNTICK="[\e[32m   \e[0m]"
-SLEEPTIME=7
+STARTSLEEPTIME=30
 
 echo -e " ${TICK} \e[32m PiHole sync starting... \e[0m"
 
 if [ "$1" == "full" ]; then
   echo -ne " ${UNTICK} \e[32m Adding remote domains to whitelist. \e[0m\033[0K\r"
-  # Comment one or more out if you do not wish to use any of these.
+  # Comment out one or more curl lines before if you do not wish to use any of these during the 'full' sync.
   curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt | sudo tee -a "${PIHOLEDIR}"/whitelist.txt >/dev/null
   curl -sS https://raw.githubusercontent.com/Ultimate-Hosts-Blacklist/DShield.org-Suspicious-Domain-List/master/whitelisted.list | sudo tee -a "${PIHOLEDIR}"/whitelist.txt >/dev/null
   curl -sS https://raw.githubusercontent.com/Ultimate-Hosts-Blacklist/whitelist/master/domains.list | sudo tee -a "${PIHOLEDIR}"/whitelist.txt >/dev/null
@@ -24,17 +24,18 @@ if [ "$1" == "full" ]; then
   echo -e " ${TICK} \e[32m Removing duplicate whitelist entries. \e[0m"
 fi
 
-echo -e " ${TICK} \e[32m Syncing files to peer PiHole. \e[0m"
+echo -ne " ${UNTICK} \e[32m Sync files to peer PiHole. \e[0m\033[0K\r"
 #LOOP FOR FILE TRANSFER
 RESTART=0 # flag determine if service restart is needed
 for FILE in ${FILES[@]}
 do
   if [[ -f $PIHOLEDIR/$FILE ]]; then
+  echo -ne " ${UNTICK} \e[32m Sync files to peer PiHole. Processing file: \e[0m ${FILE} \033[0K\r"
   RSYNC_COMMAND=$(rsync -a --out-format='%i' $PIHOLEDIR/$FILE $HAUSER@$PIHOLE2:$PIHOLEDIR)
     if [ "$RSYNC_COMMAND" != "<f..t......" ] && [ "$RSYNC_COMMAND" != "" ]; then
       # rsync copied changes
       RESTART=1 # restart flagged
-      echo -e "\e[32m         - File change detected: $FILE \e[0m"
+      echo -e " ${TICK} \e[32m Sync files to peer PiHole. File changed: \e[0m ${FILE} \033[0K\r"
      # else 
        # no changes
      fi
@@ -43,11 +44,14 @@ do
   fi
 done
 
+echo -e " ${TICK} \e[32m Syncing files to peer PiHole. Done.                                    \e[0m"
+
 FILE="adlists.list"
 RSYNC_COMMAND=$(rsync -ai $PIHOLEDIR/$FILE $HAUSER@$PIHOLE2:$PIHOLEDIR)
 if [[ -n "${RSYNC_COMMAND}" ]]; then
-  # rsync copied changes, update GRAVITY
-  ssh $HAUSER@$PIHOLE2 "sudo -S pihole updateGravity >/var/log/pihole_updateGravity.log || cat /var/log/pihole_updateGravity.log
+  echo -ne " ${UNTICK} \e[32m adlists.list change detected, running updateGravity on remote peer. This can take a while. \e[0m\033[0K\r"
+  ssh $HAUSER@$PIHOLE2 "sudo -S pihole updateGravity >/var/log/pihole_updateGravity.log || cat /var/log/pihole_updateGravity.log"
+  echo -e " ${TICK} \e[32m adlists.list change detected, running updateGravity on remote peer.                                    \e[0m"
 # else
   # no changes
 fi
@@ -55,20 +59,21 @@ fi
 if [ $RESTART == "1" ]; then
   # INSTALL FILES AND RESTART pihole
   echo -ne " ${UNTICK} \e[32m Restarting local PiHole. (Step 1 of 3) \e[0m\033[0K\r"
-  sudo -S service pihole-FTL stop
+#  sudo -S service pihole-FTL stop
   echo -ne " ${UNTICK} \e[32m Restarting local PiHole. (Step 2 of 3) \e[0m\033[0K\r"
-  sudo -S pkill pihole-FTL
+#  sudo -S pkill pihole-FTL
   echo -ne " ${UNTICK} \e[32m Restarting local PiHole. (Step 3 of 3) \e[0m\033[0K\r"
-  sudo -S service pihole-FTL start
+#  sudo -S service pihole-FTL start
   echo -e " ${TICK} \e[32m Restarting local pihole...Done.               \e[0m"
 
-  echo -ne " ${UNTICK} \e[32m Sleeping for $SLEEPTIME second for client health. Remaining: $SLEEPTIME \e[0m\033[0K\r"
+  SLEEPTIME=${STARTSLEEPTIME}
+  echo -ne " ${UNTICK} \e[32m Sleeping for ${STARTSLEEPTIME} second for client health. Remaining: \e[0m ${SLEEPTIME} \033[0K\r"
   while [ $SLEEPTIME -gt 0 ]; do
-     echo -ne " ${UNTICK} \e[32m Sleeping for $SLEEPTIME second for client health. Remaining: \e[0m$SLEEPTIME\033[0K\r"
+     echo -ne " ${UNTICK} \e[32m Sleeping for ${STARTSLEEPTIME} second for client health. Remaining: \e[0m ${SLEEPTIME} \033[0K\r"
      sleep 1
      : $((SLEEPTIME--))
   done
-  echo -e " ${TICK} \e[32m Sleeping for $SLEEPTIME second for client health. Remaining: Done         \e[0m"
+  echo -e " ${TICK} \e[32m Sleeping for $SLEEPTIME second for client health. Remaining: Done                  \e[0m"
 
   echo -ne " ${UNTICK} \e[32m Restarting peer PiHole. (Step 1 of 3) \e[0m\033[0K\r"
   ssh $HAUSER@$PIHOLE2 "sudo -S service pihole-FTL stop"
